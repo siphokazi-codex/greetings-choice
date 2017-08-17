4a249b39612821dda70a3b7bdf22627017c4cc58
siphokazi-codex <aplenis@webmail.co.za>
2017-08-17 16:07:32 +0200

    Updated changes on the index.js file - removed host

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ app.get('/greetings/counter', greetedRoutes.counter);
 
 //start the server
 var server = app.listen(process.env.PORT || 3000, function(){
-var host = server.address().address;
+//var host = server.address().address;
 var port = server.address().port;
 
-  console.log("App listening athttp://%s:%s", host, port);
+  console.log("App listening at http://%s:%s", port);
 });
